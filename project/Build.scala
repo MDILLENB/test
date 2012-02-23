efbe0e354537af5dd50a0563796f5f36078fddea
Marco Dillenburg <marco.dillenburg@bad-bertrich.net>
2012-02-23 01:05:26 +0100

    init

---
+++ project/Build.scala
@@ -0,0 +1,28 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "where-is-felipe2"
+    val appVersion      = "1.0"
+
+    val appDependencies = Seq(
+      "postgresql" % "postgresql" % "9.0-801.jdbc4",
+	  "mysql" % "mysql-connector-java" % "5.1.18",
+      "net.liftweb" %% "lift-json" % "2.4-M5",
+      "net.liftweb" %% "lift-json-ext" % "2.4-M5",
+      "com.thoughtworks.paranamer" % "paranamer-maven-plugin" % "2.2.1",
+      "org.apache.lucene" % "lucene-spatial" % "2.9.3"
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      // Add your own project settings here
+        
+        resolvers ++= Seq(
+        		Resolver.url("Play@mandubian", url("http://ci.mandubian.com/ivy-releases/"))(Resolver.ivyStylePatterns)
+		)
+		
+    )
+
+}
